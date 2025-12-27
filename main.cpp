3296fa8e59b44492931613aef7c8d8a8c47357d7
zanoxg <rusyaaa566@gmail.com>
2025-12-27 16:49:02 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,72 @@
 #include <iostream>
-#include <vector>
-#include <set>
+#include <cstdlib>
+#include <ctime>
 
-// Задание 1: Подсчет чисел с одинаковыми цифрами в диапазоне от 100 до 999
-int countNumbersWithSameDigits() {
-    int count = 0;
-    
-    for (int num = 100; num <= 999; ++num) {
-        int hundreds = num / 100;
-        int tens = (num / 10) % 10;
-        int ones = num % 10;
-        
-        if (hundreds == tens || tens == ones || hundreds == ones) {
-            ++count;
-        }
+// Задание 1: Копирование элементов из одного массива в два массива по 5 элементов
+void copyArray(int* src, int* dest1, int* dest2) {
+    for (int i = 0; i < 5; ++i) {
+        dest1[i] = src[i];
+        dest2[i] = src[i + 5];
     }
-    
-    return count;
 }
 
-// Задание 2: Подсчет чисел с разными цифрами в диапазоне от 100 до 999
-int countNumbersWithDifferentDigits() {
-    int count = 0;
-    
-    for (int num = 100; num <= 999; ++num) {
-        int hundreds = num / 100;
-        int tens = (num / 10) % 10;
-        int ones = num % 10;
-        
-        if (hundreds != tens && tens != ones && hundreds != ones) {
-            ++count;
-        }
+// Задание 2: Поэлементная сумма двух массивов с занесением результата в третий
+void sumArrays(int* arr1, int* arr2, int* result, int size) {
+    for (int i = 0; i < size; ++i) {
+        result[i] = arr1[i] + arr2[i];
     }
-    
-    return count;
-}
-
-// Задание 3: Удаление цифр 3 и 6 из введенного числа
-int removeDigits36(int num) {
-    int result = 0;
-    int place = 1;
-    
-    while (num > 0) {
-        int digit = num % 10;
-        num /= 10;
-        
-        if (digit != 3 && digit != 6) {
-            result += digit * place;
-            place *= 10;
-        }
-    }
-    
-    return result;
 }
 
 int main() {
-    // Задание 1: Подсчет чисел с одинаковыми цифрами
-    std::cout << "Count of numbers with same digits: " << countNumbersWithSameDigits() << std::endl;
+    srand(time(0));
+
+    // Задание 1: Генерация и копирование элементов массива
+    int array1[10];
+    for (int i = 0; i < 10; ++i) {
+        array1[i] = rand() % 100;  // Сгенерировать число в диапазоне от 0 до 99
+    }
     
-    // Задание 2: Подсчет чисел с разными цифрами
-    std::cout << "Count of numbers with different digits: " << countNumbersWithDifferentDigits() << std::endl;
+    int array2[5], array3[5];
+    copyArray(array1, array2, array3);
+
+    std::cout << "Original Array: ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << array1[i] << " ";
+    }
+    std::cout << "\nArray 1 (First 5 elements): ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << array2[i] << " ";
+    }
+    std::cout << "\nArray 2 (Last 5 elements): ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << array3[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // Задание 2: Генерация двух массивов и поэлементная сумма
+    int array4[5], array5[5], result[5];
+    for (int i = 0; i < 5; ++i) {
+        array4[i] = rand() % 100;
+        array5[i] = rand() % 100;
+    }
+
+    sumArrays(array4, array5, result, 5);
+
+    std::cout << "\nArray 4: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << array4[i] << " ";
+    }
     
-    // Задание 3: Удаление цифр 3 и 6
-    int number;
-    std::cout << "Enter a number: ";
-    std::cin >> number;
+    std::cout << "\nArray 5: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << array5[i] << " ";
+    }
     
-    std::cout << "Number after removing 3 and 6: " << removeDigits36(number) << std::endl;
-    
+    std::cout << "\nSum of Array 4 and Array 5: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
